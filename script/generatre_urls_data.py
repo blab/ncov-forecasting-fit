924021ff60c7505b4e52e4bd3e79251a7c13b7b7
Eslam Abousamra <eslamabousamra@eslams-mbp.fhcrc.org>
2022-06-27 14:10:34 -0700

    newscript

--- script/generatre_urls_data.py
+++ script/generatre_urls_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[34]:
+# In[10]:
 
 
 import fileinput
@@ -10,45 +10,41 @@ import git
 from git import repo
 import pandas as pd
 import os
+import argparse
 
 
-# In[ ]:
+# In[11]:
 
 
-####obtaining commits for case counts
+BASE_URL_CASE = "https://raw.githubusercontent.com/blab/rt-from-frequency-dynamics/commitid/data/omicron-countries-split/omicron-countries-split_location-case-counts.tsv"
+BASE_URL_SEQ = "https://raw.githubusercontent.com/blab/rt-from-frequency-dynamics/commitid/data/omicron-countries-split/omicron-countries-split_location-variant-sequence-counts.tsv"
 
-g = git.Git("../rt-from-frequency-dynamics")
-full_commit = list(g.log("--follow","--pretty=short", "--", "data/omicron-countries-split/omicron-countries-split_location-case-counts.tsv").split('\n'))
+parser = argparse.ArgumentParser()
+parser.add_argument("--location", type=str, help="Repo-location")
+parser.add_argument("--output", type =str, help="output-location")
+args = parser.parse_args()
+
+####obtaining commits for case counts and seqs
+g = git.Git(args.location)
 commits = list(g.log("--follow","--pretty=%H", "--", "data/omicron-countries-split/omicron-countries-split_location-case-counts.tsv").split('\n'))
 dates = list(g.log("--follow","--pretty=%as", "--", "data/omicron-countries-split/omicron-countries-split_location-case-counts.tsv").split('\n'))
 
-
-# In[ ]:
-
-
-BASE_URL_case = "https://raw.githubusercontent.com/blab/rt-from-frequency-dynamics/commitid/data/omicron-countries-split/omicron-countries-split_location-case-counts.tsv"
-BASE_URL_seq = "https://raw.githubusercontent.com/blab/rt-from-frequency-dynamics/commitid/data/omicron-countries-split/omicron-countries-split_location-variant-sequence-counts.tsv"
-
-
-# In[50]:
-
-
+#obtaining datasets and date_stamping output files
 for date, commit in zip(dates, commits):
-    # Make folder for this date
-    path = "data/" + date
+    #making folder
+    path = args.output + date
     if not os.path.exists(path):
         os.mkdir(path)
+
     # get case url
-    commit_url_case = BASE_URL_case.replace('commitid',commit)
+    commit_url_case = BASE_URL_CASE.replace('commitid',commit)
     # get seq url
-    commit_url_seq = BASE_URL_seq.replace('commitid',commit)
+    commit_url_seq = BASE_URL_SEQ.replace('commitid',commit)
     
     # save both in folder
-    this_df = pd.read_csv(commit_url_case, sep="\t")
-    this_df.to_csv(f"./data/{date}/case-counts_{date}.tsv", index=False, sep="\t")
-    other_df = pd.read_csv(commit_url_seq, sep="\t")
-    other_df.to_csv(f"./data/{date}/seq-counts_{date}.tsv", index=False, sep="\t")
-    
-    
+    case_df = pd.read_csv(commit_url_case, sep="\t")
+    case_df.to_csv(f"{path}/case-counts_{date}.tsv", index=False, sep="\t")
+    seq_df = pd.read_csv(commit_url_seq, sep="\t")
+    seq_df.to_csv(f"{path}/seq-counts_{date}.tsv", index=False, sep="\t")
     
 
