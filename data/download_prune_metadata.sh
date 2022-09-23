14be02c8f0aa3fd6d5a6609e0d2cf00e77c13a0b
marlinfiggins <marlinfiggins@gmail.com>
2022-09-23 11:00:23 -0700

    Data README and script

---
+++ data/download_prune_metadata.sh
@@ -0,0 +1,3 @@
+nextstrain remote download s3://nextstrain-ncov-private/metadata.tsv.gz
+gzip -d metadata.tsv.gz -c > gisaid_metadata.tsv
+tsv-select -H -f strain,date,country,division,QC_overall_status,Nextstrain_clade gisaid_metadata.tsv > gisaid_metadata_pruned.tsv
